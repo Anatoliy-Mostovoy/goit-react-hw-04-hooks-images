3d5aa120a5c22cbf90901cfccc1604cce3a70781
Anatoliy Mostovoy <mostovoyanatoliy@gmail.com>
2021-08-02 13:41:31 +0300

    done searchbar

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -1,39 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import s from './Searchbar.module.css';
 
-export class Searchbar extends Component {
-  state = {
-    inputValue: '',
-  };
+export const Searchbar = ({ onSubmit }) => {
+  const [inputValue, setInputValue] = useState('');
 
-  handleInputChange = event => {
+  const handleInputChange = event => {
     const { value } = event.target;
-    this.setState({ inputValue: value });
+    setInputValue(value);
   };
 
-  handleSubmit = event => {
-    const { inputValue } = this.state;
+  const handleSubmit = event => {
     event.preventDefault();
-    this.props.onSubmit(inputValue);
-    this.setState({ inputValue: '' });
+    onSubmit(inputValue);
+    setInputValue('');
   };
 
-  render() {
-    return (
-      <header className={s.Searchbar}>
-        <form className={s.SearchForm} onSubmit={this.handleSubmit}>
-          <button className={s.SearchFormButton} type="submit"></button>
-          <label>
-            <input
-              className={s.SearchFormInput}
-              type="text"
-              value={this.state.inputValue}
-              onChange={this.handleInputChange}
-              placeholder="Search images and photos"
-            />
-          </label>
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className={s.Searchbar}>
+      <form className={s.SearchForm} onSubmit={handleSubmit}>
+        <button className={s.SearchFormButton} type="submit"></button>
+        <label>
+          <input
+            className={s.SearchFormInput}
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            placeholder="Search images and photos"
+          />
+        </label>
+      </form>
+    </header>
+  );
+};
